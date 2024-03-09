28bdac20a4a9ff8a2d1c099c3d2cc61f92066406
Manan Gulati <manangulati9@gmail.com>
2024-03-09 23:48:57 +0530

    added post apis

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,39 +1,70 @@
 import { z } from "zod";
 import {
   createTRPCRouter,
-  getServerSession,
   protectedProcedure,
 } from "@/server/api/trpc";
-import { posts } from "@/server/db/schema";
+import { comments, likes, postImages, posts } from "@/server/db/schema";
+import { createClient } from "@/server/auth/server";
+import { nanoid } from 'nanoid'
+import { TRPCError } from "@trpc/server";
+import { count, eq } from "drizzle-orm";
 
 export const postRouter = createTRPCRouter({
   getAllPosts: protectedProcedure.input(z.number()).query(async ({ ctx, input }) => {
-    try {
-      const postsArr = await ctx.db.select().from(posts).limit(10).offset(input * 10);
-
-      return postsArr;
-    } catch (error) {
-      console.error(error)
-      return null;
-    }
+    const postsArr = await ctx.db.select().from(posts).limit(10).offset(input * 10);
+    return postsArr;
   }),
 
-  createPost: protectedProcedure.input(z.string()).mutation(async ({ ctx, input }) => {
-    try {
-      const session = await getServerSession();
+  createPost: protectedProcedure.input(z.object({ content: z.string(), imageBlob: z.instanceof(File) })).mutation(async ({ ctx, input }) => {
+    const userId = ctx.user.id;
+    const fileId = nanoid();
+    let path = "";
+    const fileExtension = input.imageBlob.name.split('.').pop()?.toLowerCase();
 
-      if (!session) {
-        return null;
-      }
-
-      const userId = session.user.id;
-      await ctx.db.insert(posts).values({ content: input, authorId: userId })
-
-      return true;
-    } catch (error) {
-      console.error(error)
-      return null;
+    if (input.imageBlob.type.startsWith("image/")) {
+      path = `images/${userId}/${fileId}.${fileExtension}`
+    } else if (input.imageBlob.type === 'video/mp4') {
+      path = `videos/${userId}/${fileId}.${fileExtension}`
+    } else {
+      throw new TRPCError({ message: "File type isn't supported", code: "UNPROCESSABLE_CONTENT" })
     }
+
+    const supabase = createClient();
+    const { error, data } = await supabase.storage.from("post-files").upload(path, input.imageBlob)
+
+    if (error) {
+      throw new TRPCError({ message: "File upload failed", code: "INTERNAL_SERVER_ERROR" })
+    }
+
+    if (!data) {
+      throw new TRPCError({ message: "Couldn't get file path", code: "NOT_FOUND" })
+    }
+
+    const newPosts = await ctx.db.insert(posts).values({ content: input.content, authorId: userId }).returning({ id: posts.id });
+
+    if (!newPosts[0]) {
+      throw new TRPCError({ message: "Couldn't get post id", code: "NOT_FOUND" });
+    }
+
+    await ctx.db.insert(postImages).values({ url: data.path, postId: newPosts[0].id })
+  }
+  ),
+
+  likePost: protectedProcedure.input(z.number()).mutation(async ({ ctx, input }) => {
+    await ctx.db.insert(likes).values({ authorId: ctx.user.id, postId: input, })
   }),
 
+  deletePost: protectedProcedure.input(z.number()).mutation(async ({ ctx, input }) => {
+    await ctx.db.delete(posts).where(eq(posts.id, input));
+  }),
+
+  createComment: protectedProcedure.input(z.object({ content: z.string(), postId: z.number() })).mutation(async ({ ctx, input }) => {
+    const [checkPostId] = await ctx.db.select({ value: count() }).from(posts).where(eq(posts.id, input.postId));
+
+    if (checkPostId?.value === 1) {
+      await ctx.db.insert(comments).values({ content: input.content, postId: input.postId, userId: ctx.user.id })
+      const [updatedCommentCount] = await ctx.db.select({ value: count() }).from(comments).where(eq(posts.id, input.postId)).groupBy(posts.id);
+      return updatedCommentCount ? updatedCommentCount.value : null;
+    }
+  })
 });
