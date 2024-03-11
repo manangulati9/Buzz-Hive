90b303c7a3ac931ee0d07872609cd56aeafce610
Manan Gulati <manangulati9@gmail.com>
2024-03-11 17:02:11 +0530

    using base url func

--- next.config.js
+++ next.config.js
@@ -2,10 +2,7 @@
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
  * for Docker builds.
  */
-const { env } = await import("./src/env.js");
-import dotenvExpand from "dotenv-expand"
-
-dotenvExpand.expand({ parsed: { ...env } });
+await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
 const config = {};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getBaseURL() {
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "production")
+    return "https://buzz-hive-rosy.vercel.app";
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === "preview")
+    return `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`;
+  return "http://localhost:3000";
+}
