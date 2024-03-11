0885cef514d478d1431e614ef0b6bf7a9e36fedb
Manan Gulati <manangulati9@gmail.com>
2024-03-11 14:51:52 +0530

    fixed auth errors

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,3 +1,19 @@
-export default async function Home() {
-  return <div>Hello World</div>
+'use client'
+
+import { Button } from "@/components/ui/button";
+import { createClient } from "@/server/auth/client";
+import { useRouter } from "next/navigation";
+
+export default function Home() {
+  const router = useRouter();
+
+  const handleSignOut = async () => {
+    const supabase = createClient();
+    await supabase.auth.signOut();
+  }
+
+  return <div>
+    <Button onClick={handleSignOut}>Sign out</Button>
+    <Button onClick={() => router.push('/api/auth/callback')}>Callback</Button>
+  </div>
 }
