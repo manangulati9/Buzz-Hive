d3d3943cd8246b877692a33afe1f6b7d91c8709e
Saharsh Raj <saharshraj10@gmail.com>
2024-03-14 04:44:09 +0530

    Improved Buzz

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,67 +1,71 @@
-import PostCard from '@/components/dashboard/PostCard'
-import { MessageCircle, Stars } from 'lucide-react'
-import React from 'react'
-import MessageTab from '@/components/dashboard/MessageTab';
-import { ScrollArea } from "@/components/ui/scroll-area"
-import Link from 'next/link';
-import PostBuzz from '@/components/dashboard/PostBuzz'
-import { api } from '@/trpc/server';
+import PostCard from "@/components/dashboard/PostCard";
+import { MessageCircle, Stars } from "lucide-react";
+import React from "react";
+import MessageTab from "@/components/dashboard/MessageTab";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import Link from "next/link";
+import PostBuzz from "@/components/dashboard/PostBuzz";
+import { api } from "@/trpc/server";
 import { unstable_noStore as noStore } from "next/cache";
 
 export default async function Page() {
   noStore();
-  const posts = await api.posts.getAllPosts.query({ page: 1 })
+  const posts = await api.posts.getAllPosts.query({ page: 1 });
 
   const messages = [
     {
       username: "Olivia dennis",
       userlogo: "/logo.svg",
       text: "Hi, let's have a meeting tomorrow to discuss the project. I've been reviewing the project details and have some ideas I'd like to share. It's crucial that we align on our next step to ensure the project's success. Please come prepared with any questions or insights you may have. Looking forward to our meeting! Best, Olivia",
-      verified: true
+      verified: true,
     },
     {
       username: "Max Leiter",
       userlogo: "/logo.svg",
       text: "Hey everyone! 👋 We're excited to announce that we've just closed our Series A funding round. 🚀 Thanks to all our supporters and investors. We're looking forward to the next phase of our journey! #startuplife",
-      verified: false
+      verified: false,
     },
     {
       username: "Jared Palmer",
       userlogo: "/logo.svg",
       text: "Hi, let's have a meeting tomorrow to discuss the project. I've been reviewing the project details and have some ideas I'd like to share. It's crucial that we align on our next step to ensure the project's success. Please come prepared with any questions or insights you may have. Looking forward to our meeting! Best, Olivia",
-      verified: true
+      verified: true,
     },
-  ]
+  ];
 
   return (
-    <div className='min-h-screen mx-auto justify-around py-10 text-foreground flex container w-full max-w-none relative'>
-      <div className=' w-full  '>
-        <div className='mx-auto w-[18rem]  md:w-full max-w-xl  h-fit drop-shadow-[0_0_35px_rgba(1,1,1,1.25)]  rounded-xl flex  justify-between py-2 px-6 my-5 md:backdrop-blur-3xl bg-[#1F2937] bg-opacity-50'>
-          <p className='font-bold'>
-            Latest Buzz
-          </p>
-          <Stars color='var(--primary)' />
+    <div className="container relative mx-auto flex min-h-screen w-full max-w-none justify-around py-10 text-foreground">
+      <div className=" w-full  ">
+        <div className="mx-auto my-5  flex h-fit  w-[18rem] max-w-xl  justify-between rounded-xl  bg-[#1F2937] bg-opacity-50 px-6 py-2 drop-shadow-[0_0_35px_rgba(1,1,1,1.25)] md:w-full md:backdrop-blur-3xl">
+          <p className="font-bold">Latest Buzz</p>
+          <Stars color="var(--primary)" />
         </div>
         <PostBuzz />
-        <div className="flex flex-col space-y-14">{posts.map((post) => (
-          <PostCard key={post.id} post={post} />
-        ))}</div>
+        <div className="flex flex-col space-y-14">
+          {posts.map((post) => (
+            <PostCard key={post.id} post={post} />
+          ))}
+        </div>
       </div>
-      <div className='md:flex flex-col items-center hidden fixed right-8  md:w-full max-w-sm'>
-        <div className=' mx-auto w-[20rem] md:w-full max-w-2xl    h-fit drop-shadow-[0_0_35px_rgba(1,1,1,1.25)]  rounded-xl flex  justify-between py-2 px-6 my-5 backdrop-blur-3xl bg-[#1F2937] bg-opacity-50'>
-          <p className='font-bold'>
-            Messages
-          </p>
-          <MessageCircle color='var(--primary)' />
+      <div className="fixed right-8 hidden max-w-sm flex-col items-center  md:flex md:w-full">
+        <div className=" mx-auto my-5 flex h-fit    w-[20rem] max-w-2xl  justify-between rounded-xl  bg-[#1F2937] bg-opacity-50 px-6 py-2 drop-shadow-[0_0_35px_rgba(1,1,1,1.25)] backdrop-blur-3xl md:w-full">
+          <p className="font-bold">Messages</p>
+          <MessageCircle color="var(--primary)" />
         </div>
         <ScrollArea className="h-[400px] w-full p-3">
-          <div className="flex flex-col  space-y-10 my-16">
+          <div className="my-16 flex  flex-col space-y-10">
             {messages?.map((message) => (
               <MessageTab key={message.username} message={message} />
             ))}
-          </div></ScrollArea>
-        <Link href={'/dashboard/messages'} className='px-4 py-2 border-2 rounded-2xl hover:bg-muted-foreground transition-colors duration-500'>Show All</Link>
+          </div>
+        </ScrollArea>
+        <Link
+          href={"/dashboard/messages"}
+          className="rounded-2xl border-2 px-4 py-2 transition-colors duration-500 hover:bg-muted-foreground"
+        >
+          Show All
+        </Link>
       </div>
     </div>
-  )
+  );
 }
--- src/components/dashboard/PostBuzz.tsx
+++ src/components/dashboard/PostBuzz.tsx
@@ -1,6 +1,6 @@
-"use client"
+"use client";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Dialog,
   DialogContent,
@@ -9,23 +9,23 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from "@/components/ui/dialog"
+} from "@/components/ui/dialog";
 import {
   Select,
   SelectContent,
   SelectItem,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
-import { EarthIcon, Smile } from "lucide-react"
-import Image from "next/image"
-import { type ChangeEvent, useRef, useState } from "react"
-import { Textarea } from "../ui/textarea"
+} from "@/components/ui/select";
+import { EarthIcon, Smile } from "lucide-react";
+import Image from "next/image";
+import { type ChangeEvent, useRef, useState } from "react";
+import { Textarea } from "../ui/textarea";
 
 function PostBuzz() {
   const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
-  const [showModal, setShowModal] = useState(false)
-  const [error, setError] = useState(false)
+  const [showModal, setShowModal] = useState(false);
+  const [error, setError] = useState(false);
 
   const handleFileChange = (event: ChangeEvent<HTMLInputElement>) => {
     const files = event.target.files;
@@ -35,8 +35,8 @@ function PostBuzz() {
         setError(true);
       } else {
         const newFiles = Array.from(files).slice(0, 5 - selectedFiles.length); // Limit to remaining slots
-        setSelectedFiles(prevFiles => [...prevFiles, ...newFiles]);
-        setError(false)
+        setSelectedFiles((prevFiles) => [...prevFiles, ...newFiles]);
+        setError(false);
       }
     }
   };
@@ -44,117 +44,154 @@ function PostBuzz() {
   const handleSubmit = () => {
     // Here you can perform actions with the selected files, such as uploading them to a server
     if (selectedFiles.length > 0) {
-      console.log('Selected files:', selectedFiles);
+      console.log("Selected files:", selectedFiles);
       // Perform upload or other operations with the files
     } else {
-      console.log('No files selected');
+      console.log("No files selected");
     }
-    setShowModal(false)
+    setShowModal(false);
   };
 
   const inputRef = useRef<HTMLInputElement | null>(null);
   const handleInputClick = () => {
     inputRef.current?.click();
-  } 
+  };
 
   return (
     <div>
-      <Dialog open={showModal} onOpenChange={()=> {setShowModal((prev) => !prev)}}>
-        <div className='mx-auto w-[18rem]  md:w-full max-w-xl h-fit drop-shadow-[0_0_35px_rgba(1,1,1,1.25)]  rounded-lg flex flex-col justify-between py-2 px-2 space-y-4 my-10 md:backdrop-blur-3xl bg-[#1F2937] bg-opacity-50'>
-          <div className='flex flex-col space-y-2'>
-            <div className='flex items-center text-center justify-start space-x-2'>
-              <div className='relative w-14 h-14 rounded-full'>
-                <Image src={'/logo.svg'} alt={''} fill />
+      <Dialog
+        open={showModal}
+        onOpenChange={() => {
+          setShowModal((prev) => !prev);
+        }}
+      >
+        <div className="mx-auto my-10  flex h-fit w-[18rem] max-w-xl  flex-col justify-between space-y-4 rounded-lg bg-[#1F2937] bg-opacity-50 px-2 py-2 drop-shadow-[0_0_35px_rgba(1,1,1,1.25)] md:w-full md:backdrop-blur-3xl">
+          <div className="flex flex-col space-y-2">
+            <div className="flex items-center justify-start space-x-2 text-center">
+              <div className="relative h-14 w-14 rounded-full">
+                <Image src={"/logo.svg"} alt={""} fill />
               </div>
-              <p className='font-bold text-base'>
-                What&apos; Buzzin?
-              </p>
+              <p className="text-base font-bold">What&apos; Buzzin?</p>
             </div>
 
-            <div className='md:pl-16 pl-8'>
+            <div className="pl-8 md:pl-16">
               <Select>
-                <SelectTrigger className="w-fit bg-primary text-[#322904] rounded-xl text-xs md:text-base hover:bg-yellow-500 transition-colors duration-500">
-                  <SelectValue placeholder="Select privacy" className='placeholder:font-bold' />
+                <SelectTrigger className="w-fit rounded-xl bg-primary text-xs text-[#322904] transition-colors duration-500 hover:bg-yellow-500 md:text-base">
+                  <SelectValue
+                    placeholder="Select privacy"
+                    className="placeholder:font-bold"
+                  />
                 </SelectTrigger>
                 <SelectContent>
-                  <SelectItem value="EveryOne" >
-                    <div className='flex space-x-3 text-[#322904]  items-center'>
-
-                      <EarthIcon className='text-[#322904]' />
-                      <p className='font-bold'>
-                        Everyone can comment
-
-                      </p>
+                  <SelectItem value="EveryOne">
+                    <div className="flex items-center space-x-3  text-[#322904]">
+                      <EarthIcon className="text-[#322904]" />
+                      <p className="font-bold">Everyone can comment</p>
                     </div>
                   </SelectItem>
                   <SelectItem value="OnlyFollowers">
-                    <div className='flex space-x-3 text-[#322904]  items-center'>
-
-                      <EarthIcon className='text-[#322904]' />
-                      <p className='font-bold'>
-                        Only followers
-
-                      </p>
+                    <div className="flex items-center space-x-3  text-[#322904]">
+                      <EarthIcon className="text-[#322904]" />
+                      <p className="font-bold">Only followers</p>
                     </div>
                   </SelectItem>
                   <SelectItem value="OnlyYouFollow">
-                    <div className='flex space-x-3 text-[#322904]  items-center'>
-
-                      <EarthIcon className='text-[#322904]' />
-                      <p className='font-bold'>
-                        Only people you follow
-
-                      </p>
+                    <div className="flex items-center space-x-3  text-[#322904]">
+                      <EarthIcon className="text-[#322904]" />
+                      <p className="font-bold">Only people you follow</p>
                     </div>
                   </SelectItem>
                 </SelectContent>
               </Select>
-
             </div>
 
-            <div className='flex justify-between md:pl-16 pl-10 items-center border-t-2 p-2 border-yellow-400 '>
-              <div className='flex justify-evenly space-x-2'>
-                <DialogTrigger><Image src={'/mediaicon.svg'} alt={'media'} width={23} height={23} className='hover:scale-110 transition-all duration-200 cursor-pointer' /></DialogTrigger>
-                <DialogTrigger><Smile className='text-primary hover:scale-110 transition-all duration-200 cursor-pointer' height={23} width={23} /></DialogTrigger>
+            <div className="flex items-center justify-between border-t-2 border-yellow-400 p-2 pl-10 md:pl-16 ">
+              <div className="flex justify-evenly space-x-2">
+                <DialogTrigger>
+                  <Image
+                    src={"/mediaicon.svg"}
+                    alt={"media"}
+                    width={23}
+                    height={23}
+                    className="cursor-pointer transition-all duration-200 hover:scale-110"
+                  />
+                </DialogTrigger>
+                <DialogTrigger>
+                  <Smile
+                    className="cursor-pointer text-primary transition-all duration-200 hover:scale-110"
+                    height={23}
+                    width={23}
+                  />
+                </DialogTrigger>
               </div>
               <DialogTrigger asChild>
-                <div className='border-2 flex items-center justify-center w-fit h-fit bg-primary hover:bg-yellow-500 transition-colors duration-500 rounded-full px-4 py-1 cursor-pointer text-[#322904]'>
-                  <p className='font-semibold text-sm md:text-base Buzz'>Buzz</p>
+                <div className="flex h-fit w-fit cursor-pointer items-center justify-center rounded-full border-2 bg-primary px-4 py-1 text-[#322904] transition-colors duration-500 hover:bg-yellow-500">
+                  <p className="Buzz text-sm font-semibold md:text-base">
+                    Buzz
+                  </p>
                 </div>
               </DialogTrigger>
             </div>
           </div>
-          <DialogContent className="max-w-[24rem] sm:max-w-[425px] bg-[#030712] text-foreground border-primary rounded-lg">
+          <DialogContent className="max-w-[400px] rounded-lg border-primary bg-[#030712] text-foreground md:max-w-[40rem]">
             <DialogHeader>
-              <DialogTitle>Buzz on BuzzHive</DialogTitle>
+              <DialogTitle className="text-2xl">Create some Buzz</DialogTitle>
               <DialogDescription>
                 Share what you want with users across BuzzHive!
               </DialogDescription>
             </DialogHeader>
-            <div className="flex flex-col space-y-4  py-4  rounded-xl md:p-4">
-              <div className="">
-                <Textarea className="border-muted-foreground border-2" />
+            <div className="flex flex-col gap-2  space-y-4  rounded-xl py-4">
+              <Textarea
+                className="min-h-40 border-2 border-muted-foreground md:min-h-80"
+                placeholder="What's on your mind today?"
+              />
+
+              <div className="flex items-center">
+                <Button
+                  className="h-fit w-fit justify-around gap-4 border-2 border-muted-foreground bg-transparent text-foreground hover:bg-muted-foreground"
+                  onClick={handleInputClick}
+                >
+                  <Image
+                    src={"/mediaicon.svg"}
+                    alt={""}
+                    width={20}
+                    height={20}
+                  />
+                  Upload
+                </Button>
+                <input
+                  type="file"
+                  accept="image/*,video/mp4"
+                  multiple
+                  onChange={handleFileChange}
+                  ref={inputRef}
+                  className="hidden"
+                />
               </div>
-              <div  className="flex justify-center items-center">
-              
-              <Button className="text-foreground bg-transparent border-muted-foreground border-2 hover:bg-muted-foreground w-fit h-fit" onClick={handleInputClick}> 
-            
-               Upload
-              
-              </Button>
-              <input type="file" accept="image/*,video/mp4" multiple onChange={handleFileChange} ref={inputRef} className="hidden"/>
-              </div>
-            {error && <p className="text-center text-destructive text-sm">You can only upload up to five files. Try again</p> }
-            {selectedFiles.length>0 && <p className="text-center text-primary text-sm">{`${selectedFiles.length} files uploaded`}</p> }
+              {error && (
+                <p className="text-center text-sm text-destructive">
+                  You can only upload up to five files. Try again
+                </p>
+              )}
+              {selectedFiles.length > 0 && (
+                <p className="text-center text-sm text-primary">{`${selectedFiles.length} files uploaded`}</p>
+              )}
             </div>
             <DialogFooter>
-              <Button type="submit" onClick={handleSubmit} disabled={error}>Buzz</Button>
+              <Button
+                type="submit"
+                onClick={handleSubmit}
+                disabled={error}
+                className="font-bold"
+              >
+                Buzz!
+              </Button>
             </DialogFooter>
           </DialogContent>
         </div>
       </Dialog>
-    </div>  
-  )
+    </div>
+  );
 }
 
-export default PostBuzz
+export default PostBuzz;
